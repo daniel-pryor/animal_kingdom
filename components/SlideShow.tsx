e6adcba322a55c3702500d51174b6cbaef97661e
daniel-pryor <danpryor13@gmail.com>
2023-08-17 17:02:38 +1000

    update input

--- components/SlideShow.tsx
+++ components/SlideShow.tsx
@@ -59,14 +59,14 @@ const Slideshow = () => {
                   <input
                     className='appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none'
                     type='text'
-                    placeholder='Email...'
+                    placeholder='Join our email waitlist'
                     aria-label='Email'
                   />
                   <button
                     className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
                     type='button'
                   >
-                    Sign Up
+                    Join
                   </button>
                 </div>
               </form>
